4abfcb8d6c9ebb1fae63138da686987a2b5c7c7f
akkassingh <akkassingh@gmail.com>
2024-07-21 18:38:25 -0400

    setting up GraphQL Server

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import express from 'express';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./data/schema";
 
 const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('GraphQL is amazing!');
+app.get("/", (req, res) => {
+  res.send("GraphQL is amazing!");
 });
 
+const root = {
+  hello: () => "Hello world!",
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
